4cdb5e8191a286d7489d3ba4ad63407015386268
Fredy Obonyo <fredobonyo5@gmail.com>
2024-07-21 21:09:26 +0300

    Adjustments for portfolio website hackathon

---
+++ index.js
@@ -0,0 +1,3 @@
+p_one = document.getElementById('main_p');
+
+p_one.innerHTML = "I have completely changed this paragraph using javascript. It now reads <br> I am a junior software engineer whon is entering into the industries with <br>high hope and also prepared for the chanllenges in it";
